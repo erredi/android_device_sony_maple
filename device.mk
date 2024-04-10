525ea902149427b61721ef98ba8b71156994324a
erredi <64195203+erredi@users.noreply.github.com>
2024-04-10 09:17:21 +0000

    Update device.mk
    Update with mulch system webview

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Update with mulch system webview
+PRODUCT_PACKAGES += mulch-webview
+
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
