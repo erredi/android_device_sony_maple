4c133f2fb1265b4f90359a06c19108316121a09b
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2024-12-06 01:47:32 +0700

    maple: Update build props for android 15

--- lineage_maple.mk
+++ lineage_maple.mk
@@ -26,7 +26,5 @@ PRODUCT_MODEL := G8141
 PRODUCT_MANUFACTURER := Sony
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=maple \
-    PRIVATE_BUILD_DESC="G8141-user 9 47.2.A.10.107 172320177 release-keys"
-
-BUILD_FINGERPRINT := Sony/G8141/G8141:9/47.2.A.10.107/172320177:user/release-keys
+    BuildDesc="G8141-user 9 47.2.A.10.107 172320177 release-keys" \
+    BuildFingerprint=Sony/G8141/G8141:9/47.2.A.10.107/172320177:user/release-keys
