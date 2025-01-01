50c4eec9baae22af20c015149676e634dc7cff72
erredi <64195203+erredi@users.noreply.github.com>
2025-01-01 13:10:29 +0100

    Update device.mk
    remove mulch webview

--- device.mk
+++ device.mk
@@ -18,9 +18,6 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Update with mulch system webview
-PRODUCT_PACKAGES += mulch-webview
-
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
 
